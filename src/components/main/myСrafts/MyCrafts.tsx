0dad33f1de33b59ab8bb95b318d4dc90f816079b
k2 <izmail.k2@gmail.com>
2022-11-30 18:01:59 +0300

    component skill refactor. delete gap -> @media

--- src/components/main/myСrafts/MyCrafts.tsx
+++ src/components/main/myСrafts/MyCrafts.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import s from './MyCrafts.module.css';
+import {v1} from "uuid";
+import socialImg from './social.jpg';
+import appImg from './app.jpeg';
 
 export const MyCrafts = () => {
+
+    const crafts = [
+        {id: v1(), title: 'Social network', background:socialImg,  description:'Have a good mood checking'},
+        {id: v1(), title: 'Application', background:appImg,  description:'And again to you sensei! May the force be with you'},
+    ]
     return (
-        <div>
-        </div>
+        <section className={s.craft}>
+            <div className={s.containerItem}>
+                <h3 className='appGeneralTitle'>My crafts</h3>
+                <div className={s.containerCrafts}>
+                    {crafts.map(c => {
+                        return (
+                            <div className={s.containerBlock} style={{backgroundImage: `url(${c.background})`}}>
+                                <div>
+                                    <span>{c.title}</span>
+                                    <p className='appGeneralDescription'></p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
     );
 };
 
