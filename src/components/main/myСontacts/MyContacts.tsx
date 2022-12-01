a1be45e32117d3a38a9d6068430d02ce401a47ce
k2 <izmail.k2@gmail.com>
2022-12-01 23:58:39 +0300

    componenta Contacts

--- src/components/main/myСontacts/MyContacts.tsx
+++ src/components/main/myСontacts/MyContacts.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import s from './MyContacts.module.css';
 
 export const MyContacts = () => {
     return (
-        <div>
-        </div>
+        <section className={s.contact}>
+            <div className={s.container}>
+                <h3 className='appGeneralTitle'>Contacts</h3>
+                    <form action="" className={s.form}>
+                        <input type="text" placeholder='Full name'/>
+                        <input type="text" placeholder='Phone number'/>
+                        <input type="text" placeholder='E-mail'/>
+                        <textarea name="" placeholder='You message'></textarea>
+                    </form>
+                <button>Send</button>
+
+            </div>
+        </section>
     );
 };
 
