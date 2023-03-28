f0eac33c56f91f8e381bf1e3a9e8d2681c82d574
k2 <izmail.k2@gmail.com>
2023-03-28 22:33:53 +0300

    My Work

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import s from "./Button.module.scss";
+
+type ButtonType = {
+    title: string,
+}
+export const Button = (props: ButtonType) => {
+    const {title} = props;
+    const [style, setStyle] = useState('')
+
+    const off = () => {
+        setStyle(s.modOff)
+    }
+
+    return (
+        <>
+            <button className={`${style}`} onMouseOut={off}>{title}</button>
+        </>
+    );
+};
+
--- src/components/main/work/Work.tsx
+++ src/components/main/work/Work.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import s from './Work.module.css';
+import s from './Work.module.scss';
+import i from '../wrapper/I_maloy.jpg'
+import {Button} from "../../button/Button";
 
 export const Work = () => {
+
     return (
-        <section className={s.work}>
+        <section className={s.work} style={{backgroundImage: `url(${i})`}}>
             <div className={s.container}>
-                <h3 className='appGeneralTitle'>Considering remote work options</h3>
-                <button>hire me</button>
+                <h2>Do you like my work?</h2>
+                <p>Let’s work together!</p>
+                <Button title={'Get in touch'}/>
             </div>
         </section>
     );
