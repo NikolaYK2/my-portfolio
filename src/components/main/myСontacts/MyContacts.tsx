d5e4e7667db0886a2b78b0d286a8077664c093a1
k2 <izmail.k2@gmail.com>
2023-03-29 21:46:11 +0300

    My Contacts

--- src/components/main/myСontacts/MyContacts.tsx
+++ src/components/main/myСontacts/MyContacts.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import s from './MyContacts.module.css';
+import s from './MyContacts.module.scss';
+import {TitleH2} from "../../titleH2/titleH2";
+import {Button} from "../../button/Button";
 
 export const MyContacts = () => {
     return (
         <section id={'contact'} className={s.contact}>
             <div className={s.container}>
-                <h3 className='appGeneralTitle'>Contacts</h3>
+                <TitleH2 title={'Contacts'}/>
                 <form action="#" encType='#' method='#' className={s.form}>
-                    <input type="text" placeholder='Full name'/>
-                    <input type="tel" placeholder='Phone number'/>
-                    <input type="email" placeholder='E-mail'/>
-                    <textarea name="" placeholder='You message'></textarea>
-                    <button>Send</button>
+                    <div className={s.formMod}>
+                        <div className={s.formInput}>
+                            <input type="text" placeholder='Full name'/>
+                            <input type="tel" placeholder='Phone number'/>
+                            <input type="email" placeholder='E-mail'/>
+                        </div>
+                        <div className={s.formTextarea}>
+                            <textarea name="" placeholder='You message'></textarea>
+                        </div>
+                    </div>
+                    {/*<button>Send</button>*/}
+                    <Button title={'send'}/>
                 </form>
             </div>
         </section>
