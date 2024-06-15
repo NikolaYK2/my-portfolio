b29f890c8c3fb96902e055e49d2447ff381472c6
k2 <izmail.k2@gmail.com>
2024-06-15 21:56:35 +0300

    refactor: Rename object properties to match backend expectations

--- src/features/2-main/ui/myСontacts/MyContacts.tsx
+++ src/features/2-main/ui/myСontacts/MyContacts.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './MyContacts.module.scss';
 import {TitleH2} from "common/components/titleH2/titleH2";
 import {Button} from "common/components/button/Button";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
+import {apiContacts} from "features/2-main/api/myContacts/apiContacts";
+import {Snackbar} from "common/components/snackbar/Snackbar";
 
 const phoneValidation = /^(?:\+?\d{1,3})?(?:[-\s()]|\d){10,}$/;
 const messageSchema = z.object({
   name: z.string().trim().min(3, {message: 'name должен быть min 3 to max 10 litters'}).max(10),
   email: z.string().trim().email({message: 'Invalid email address'}),
   tel: z.string().regex(phoneValidation, {message: 'Invalid phone number'}).optional().or(z.literal('')),
-  message: z.string().trim().max(500, 'max message 500 litters')
+  text: z.string().trim().max(500, 'max message 500 litters')
 })
 
-type NameType = "name" | "tel" | "email" | "message"
+type NameType = "name" | "tel" | "email" | "text"
 type InputType = {
   name: NameType;
   type: string;
   placeholder: string;
 }
-type FormType = z.infer<typeof messageSchema>
+export type FormType = z.infer<typeof messageSchema>
 type MyContactsType = {
   id: string
 }
 export const MyContacts = (props: MyContactsType) => {
+  const [message, setMessage] = useState('');
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const input: InputType[] = [
     {name: 'name', type: 'text', placeholder: 'Full name',},
     {name: 'tel', type: 'tel', placeholder: 'Phone number',},
@@ -35,6 +39,7 @@ export const MyContacts = (props: MyContactsType) => {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: {errors,}
   } = useForm<FormType>({
     resolver: zodResolver(messageSchema),
@@ -42,18 +47,29 @@ export const MyContacts = (props: MyContactsType) => {
       name: '',
       tel: '',
       email: '',
-      message: ''
+      text: ''
     }
   })
+  const onSubmit: SubmitHandler<FormType> = async (data) => {
+    setIsDisabled(true)
 
-  const onSubmit: SubmitHandler<FormType> = data => {
-    console.log(data)
+    try {
+      const res = await apiContacts.sendMessage(data)
+      setMessage(res)
+      reset()
+    } catch (e) {
+
+    } finally {
+      setIsDisabled(false)
+    }
   }
+
 
   return (
     <section id={props.id} className={s.contact}>
       <div className={s.container}>
         <TitleH2 title={'Contacts'}/>
+        <Snackbar message={message} setMessage={setMessage}/>
         <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
           <div className={s.formMod}>
             <div className={s.formInput}>
@@ -78,10 +94,10 @@ export const MyContacts = (props: MyContactsType) => {
               )}
             </div>
             <div className={s.formTextarea}>
-              <textarea {...register("message")} placeholder='You message'></textarea>
+              <textarea {...register("text")} placeholder='You message'></textarea>
             </div>
           </div>
-          <Button title={'send'}/>
+          <Button title={'send'} disabled={isDisabled}/>
         </form>
       </div>
     </section>
