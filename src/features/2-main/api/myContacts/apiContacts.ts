257cdfa121cfe47f66666029685b200ea4d9bae8
k2 <izmail.k2@gmail.com>
2024-06-15 21:52:15 +0300

    feat: add apiContacts

--- src/features/2-main/api/myContacts/apiContacts.ts
+++ src/features/2-main/api/myContacts/apiContacts.ts
@@ -1,3 +1,16 @@
-export const apiContacts = {
+import {instance} from "common/api/instance";
+import {FormType} from "features/2-main/ui/myСontacts/MyContacts";
 
+type SendMessageType = {
+  message: string;
+}
+export const apiContacts = {
+  async sendMessage({text, tel, name, email}: FormType) {
+    try {
+      let res = await instance.post<SendMessageType>('/sendMessage', {text, tel, name, email});
+      return res.data.message;
+    } catch (e: any) {
+      return e.data.message
+    }
+  }
 }
