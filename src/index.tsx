6eeef16cb71f8e8f59fce906eed7ca877f15bb7f
k2 <izmail.k2@gmail.com>
2024-06-18 21:34:44 +0300

    refactor: styles connection redesigned - removed default index, now connect from the common folder of all styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'assets/SCSS/index.scss'
 import reportWebVitals from './reportWebVitals';
 import App from "app/App";
