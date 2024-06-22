69a34567fe9d2feb0c6c60eba1ab14a34655186d
k2 <izmail.k2@gmail.com>
2024-06-22 21:01:29 +0300

    refactor: remove btn

--- src/features/2-main/ui/work/Work.tsx
+++ src/features/2-main/ui/work/Work.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import s from './Work.module.scss';
 import i from 'assets/image/wrapper/I_maloy.jpg'
-import {Button} from "common/components/button/Button";
 import {animationOnScroll} from "common/utils/animateOnScroll";
 
 export const Work = () => {
@@ -10,12 +9,12 @@ export const Work = () => {
         animationOnScroll('#h2', s.activeH2)
         animationOnScroll('#p', s.activeP)
     }, []);
+
     return (
         <section className={s.work} style={{backgroundImage: `url(${i})`}}>
             <div className={s.container}>
                 <h2 id='h2'>Do you like my work?</h2>
                 <p id='p'>Let’s work together!</p>
-                <Button title={'Get in touch'}/>
             </div>
         </section>
     );
