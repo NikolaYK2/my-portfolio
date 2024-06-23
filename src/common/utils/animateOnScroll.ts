89747743f2f9f40fe11c26c499756d96157bf129
k2 <izmail.k2@gmail.com>
2024-06-23 10:38:39 +0300

    refactor: add observer disconnect

--- src/common/utils/animateOnScroll.ts
+++ src/common/utils/animateOnScroll.ts
@@ -1,14 +1,19 @@
 export const animationOnScroll = (selector: string, className: string) => {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            const element = entry.target as HTMLElement
-            if (entry.isIntersecting) {
-                element.classList.add(className);
-            } else {
-                element.classList.remove(className);
-            }
-        })
+  const observer = new IntersectionObserver((entries) => {
+
+   entries.forEach((entry) => {
+      const element = entry.target as HTMLElement
+      if (entry.isIntersecting) {
+          element.classList.add(className);
+      } else {
+        element.classList.remove(className);
+      }
     })
-    const elements = document.querySelectorAll(selector)
-    elements.forEach((el) => observer.observe(el))
+  })
+  const elements = document.querySelectorAll(selector)
+  elements.forEach((el) => observer.observe(el))
+
+  return ()=>{
+    observer.disconnect()
+  }
 }
