9ecd44f33a2a869ca55e2b7bfe415d8387007348
k2 <izmail.k2@gmail.com>
2024-06-23 16:31:36 +0300

    feat: add props for animation

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -1,11 +1,14 @@
+import {m, Variant} from 'framer-motion';
 import React, {useState} from 'react';
 import s from "./Button.module.scss";
 
 type ButtonType = {
   title: string,
   disabled?: boolean,
+  variantsAnimation?:Variant | {}
+  visible?:boolean,
 }
-export const Button = ({title, disabled}: ButtonType) => {
+export const Button = ({title, disabled, visible=true, variantsAnimation}: ButtonType) => {
   const [style, setStyle] = useState('')
 
   const off = () => {
@@ -14,7 +17,17 @@ export const Button = ({title, disabled}: ButtonType) => {
 
   return (
     <>
-      <button className={`${s.button} ${style} ${disabled && s.disabled}`} onMouseOut={off} disabled={disabled}>{title}</button>
+      <m.button
+        className={`${s.button} ${style} ${disabled && s.disabled}`}
+        onMouseOut={off}
+        disabled={disabled}
+        variants={variantsAnimation ? variantsAnimation : {}}
+        initial="hidden"
+        animate={visible ? "visible" : 'hidden'}
+
+      >
+        {title}
+      </m.button>
     </>
   );
 };
