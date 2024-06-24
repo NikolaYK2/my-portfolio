da5b234a64af467bb86e50ce2494e6423bcd911d
k2 <izmail.k2@gmail.com>
2024-06-24 12:33:40 +0300

    refactor: Passing data behind the component - Add type for taskNav - add link from react skroll for logo

--- src/features/1-header/Header.tsx
+++ src/features/1-header/Header.tsx
@@ -2,15 +2,21 @@ import React, {memo, useCallback, useEffect, useState} from 'react';
 import s from './Header.module.scss';
 import {Link} from "react-scroll";
 import {Logo} from "common/components/logo/Logo";
+import {BackToTop} from "common/components/backToTop/BackToTop";
 
-
+type TaskNavType = {
+  id: string;
+  link: string;
+  title: string;
+  color: string;
+}
+const taskNav: TaskNavType[] = [
+  {id: '1', link: 'wrap', title: 'Home', color: '#9da1a5'},
+  {id: '2', link: 'skills', title: 'Skills', color: '#9da1a5'},
+  {id: '3', link: 'crafts', title: 'Works', color: '#9da1a5'},
+  {id: '4', link: 'contact', title: 'Contacts', color: '#9da1a5'},
+]
 export const Header = memo(() => {
-  const taskNav = [
-    {id: '1', link: 'wrap', title: 'Home', color: '#9da1a5'},
-    {id: '2', link: 'skills', title: 'Skills', color: '#9da1a5'},
-    {id: '3', link: 'crafts', title: 'Works', color: '#9da1a5'},
-    {id: '4', link: 'contact', title: 'Contacts', color: '#9da1a5'},
-  ]
   const [modNav, setStyleNav] = useState(s.headerNav);
   const [modBurger, setStyleBurger] = useState('');
   const [switchNav, setSwitchNav] = useState(true);
@@ -67,7 +73,13 @@ export const Header = memo(() => {
   return (
     <section className={`${s.header} ${scrollDirection === "down" ? s.down : s.up} ${background}`}>
       <div className={s.headerContainer}>
-        <Link to="wrap"><Logo title={'Nik.'}/></Link>
+        <Link to="wrap"
+              smooth={true}
+              spy={true}
+              offset={-50}
+        >
+          <Logo title={'Nik.'}/>
+        </Link>
         <div className={`${s.menuBurger} ${modBurger}`} onClick={burgerClick}>
           <span/>
         </div>
@@ -77,6 +89,7 @@ export const Header = memo(() => {
           </ul>
         </nav>
       </div>
+      <BackToTop/>
     </section>
   );
 });
@@ -87,8 +100,7 @@ type LiType = {
   task: { id: string, link: string, title: string, color: string },
   activeLi: (id: string) => void;
 }
-const Li = memo((props: LiType) => {
-  const {task, activeLi} = props;
+const Li = memo(({task, activeLi}: LiType) => {
 
   return (
     <li>
