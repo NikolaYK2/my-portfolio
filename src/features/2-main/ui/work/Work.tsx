f4fd3cf14087d25817e887cb43f5f024a1b45cbe
k2 <izmail.k2@gmail.com>
2024-08-13 23:04:14 +0300

    chore: add new image

--- src/features/2-main/ui/work/Work.tsx
+++ src/features/2-main/ui/work/Work.tsx
@@ -1,22 +1,22 @@
 import React, {useEffect} from 'react';
 import s from './Work.module.scss';
-import i from 'assets/image/wrapper/I_maloy.jpg'
+import i from 'assets/image/wrapper/portfolio.png'
 import {animationOnScroll} from "common/utils/animateOnScroll";
 
 export const Work = () => {
 
-    useEffect(() => {
-        animationOnScroll('#h2', s.activeH2)
-        animationOnScroll('#p', s.activeP)
-    }, []);
+  useEffect(() => {
+    animationOnScroll('#h2', s.activeH2)
+    animationOnScroll('#p', s.activeP)
+  }, []);
 
-    return (
-        <section className={s.work} style={{backgroundImage: `url(${i})`}}>
-            <div className={s.container}>
-                <h2 id='h2'>Do you like my work?</h2>
-                <p id='p'>Let’s work together!</p>
-            </div>
-        </section>
-    );
+  return (
+    <section className={s.work} style={{backgroundImage: `url(${i})`}}>
+      <div className={s.container}>
+        <h2 id='h2'>Do you like my work?</h2>
+        <p id='p'>Let’s work together!</p>
+      </div>
+    </section>
+  );
 };
 
