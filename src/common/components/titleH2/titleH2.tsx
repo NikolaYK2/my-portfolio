8de5d1e5554f6c68e0ee32bfc90f3a135e4bc108
k2 <izmail.k2@gmail.com>
2025-02-02 21:45:54 +0300

    feat: add Section for animation title

--- src/common/components/titleH2/titleH2.tsx
+++ src/common/components/titleH2/titleH2.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {ReactNode, useRef} from 'react';
 import s from './titleH2.module.scss'
+import {useInView} from "framer-motion";
 
 const truncateString = (title: string): string => {
   const index = title.indexOf(' ');
@@ -13,10 +14,36 @@ export const TitleH2 = ({title}: Props) => {
 
   return (
     <div className={s.containerTitleH2}>
-      <h2 className={s.title}>{title}</h2>
-      <div className={s.backgroundTitleH2}>
+      <Section className={s.backgroundTitleH2}>
         {truncateString(title)}
-      </div>
+      </Section>
+      <Section>
+        <h2 className={s.title}>{title}</h2>
+      </Section>
     </div>
   );
 };
+
+type SectionProps = {
+  children: ReactNode;
+  className?: string;
+}
+
+function Section({children, className}: SectionProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: true});
+
+  return (
+    <section ref={ref}>
+      <div className={className}
+           style={{
+             transform: isInView ? "none" : "translateX(-100%)",
+             opacity: isInView ? 1 : 0,
+             transition: "all 1s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+           }}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
